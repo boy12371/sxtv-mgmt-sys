4ff93347b38996178dbb8dded8aef80aa5f7f577
dds810818 <dds810818@0dfd74f2-fc29-11de-a1ec-57db84049563>
2010-01-17 08:13:37 +0000

    move tabview to center

--- web/framework/js/layout.js
+++ web/framework/js/layout.js
@@ -6,6 +6,24 @@ function refreshIframe(obj){
 
 function resizeIframe(){
 	var iframeObj = document.getElementById("contentFrameId");
-	iframeObj.style.height = window.screen.availHeight - 340 + "px";
+	iframeObj.style.height = window.screen.availHeight - 300 + "px";
 	return false;
+}
+
+function resizeTabview(){
+	var iframeObj = document.getElementById("contentFrameId");
+	var tabview = document.getElementById("tabView");
+	var tabOffset = iframeObj.offsetLeft + 100;
+	tabview.style.marginLeft = tabOffset + "px";
+	
+	var subTabview = document.getElementById("subTabview");
+	subTabview.style.marginLeft = "-" + tabOffset + "px";
+	
+	var subULs = document.getElementsByTagName("ul");
+	for(var i=0,j=0;i<subULs.length;i++){
+		if("subTabUL" == subULs[i].className){
+			subULs[i].style.marginLeft = tabOffset + j*76 + "px";
+			j++
+		}
+	}
 }
